74879b918442fc217461be714ec8b6e6ad4cfed2
RaphaellaMorais <raphaellakm@gmail.com>
2022-07-19 18:51:31 -0300

    criando-primeiras-classes

---
+++ assets/typescript/calculadora/CalculadoraControle.ts
@@ -0,0 +1,14 @@
+import DataHora from "./DataHora.js";
+import Tela from "./Tela.js";
+
+export default class CalculadoraControle {
+
+    constructor(
+        private tela = new Tela()
+    ) {
+
+
+        new DataHora();
+
+    }
+}
